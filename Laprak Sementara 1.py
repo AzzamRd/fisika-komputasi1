33804c4f72f8e569e7ac52f65818a352c28bd94d
AzzamRd <91584912+AzzamRd@users.noreply.github.com>
2024-02-20 05:43:34 +0700

    Add files via upload

---
+++ Laprak Sementara 1.py
@@ -0,0 +1,34 @@
+'''
+Latihan
+Tampilkan teks berikut di terminal dan simpan file dengan nama Praktikum_1.py:
+Nama: (Isi dengan nama Anda)
+NIM: (Isi dengan NIM Anda)
+Mata Kuliah: Fisika Komputasi I
+Belajar Python sangat menyenangkan.
+Aku ingin menjadi programmer professional.
+Halo dunia, halo Python.
+'''
+print("Hallo Apa Kabar :)")
+nama = input("masukan nama anda: ")
+nim = input("masukan NIM anda: ")
+print("Hallo " + nama + " dengan NIM " + nim)
+print("Belajar Python sangat menyenangkan.","Aku ingin menjadi programmer professional.","Halo dunia, halo Python.")
+print()
+'''
+Buatlah program kalkulator Body Mass Index (BMI)
+Rumus 𝐵𝑀𝐼 =
+𝑏𝑒𝑟𝑎𝑡 𝑏𝑎𝑑𝑎𝑛
+𝑡𝑖𝑛𝑔𝑔𝑖 𝑏𝑎𝑑𝑎𝑛2
+print('Kalkulator BMI')
+print()
+berat_badan = input('Masukkan berat badan (kg): ')
+tinggi_badan = input('Masukkan tinggi badan (cm): ')
+BMI = int(berat_badan)/((int(tinggi_badan)/100)**2)
+print('Indeks Massa Tubuh Anda adalah', BMI
+'''
+print('Kalkulator BMI')
+print()
+berat_badan = input('Masukkan berat badan (kg): ')
+tinggi_badan = input('Masukkan tinggi badan (cm): ')
+BMI = int(berat_badan)/((int(tinggi_badan)/100)**2)
+print('Indeks Massa Tubuh Anda adalah', BMI)
